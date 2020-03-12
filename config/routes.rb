26377bf4f74ddb1da38952235c608598f207025d
akhilapidikiti <akhila.pidikiti9@gmail.com>
2020-03-12 19:11:15 +0530

    User management is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-    # get "todos", to: "todos#index"
-    # post "todos", to: "todos#create"
-    # get "todos/:id", to: "todos#show"
-    post "users/login", to: "users#login"
-    resources :users
+  # get "todos", to: "todos#index"
+  # post "todos", to: "todos#create"
+  # get "todos/:id", to: "todos#show"
+  post "users/login", to: "users#login"
+  resources :users
 end
