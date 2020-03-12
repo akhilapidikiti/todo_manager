28dec37a67cc853e3f62ab75bf36cb830b6861d3
akhilapidikiti <akhila.pidikiti9@gmail.com>
2020-03-12 10:09:57 +0530

    created a new user by sending a POST request to /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,17 @@ class UsersController < ApplicationController
         user=User.find(id)
         render plain: user.to_s
     end
+    def create
+        name = params[:name]
+        email = params[:email]
+        password = params[:password]
+       new_user = User.create!(
+          name: name,
+          email: email,
+          password: password,
+        )
+       response_text = "hey your new user is created with id #{new_user.id}" 
+       render plain: response_text
+     end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
     # get "todos", to: "todos#index"
     # post "todos", to: "todos#create"
     # get "todos/:id", to: "todos#show"
-    post "users/login", to: "users#login"
+    
     resources :users
 end
