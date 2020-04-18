2664f6deb2235c1502875428882540ea4b5e9693
akhilapidikiti <akhila.pidikiti9@gmail.com>
2020-04-18 13:02:23 +0530

    Add user signin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   #post "users/login", to: "users#login"
   get "todos", to: "todos#index"
   get "/" => "home#index"
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
   resources :todos
   resources :users
 end
